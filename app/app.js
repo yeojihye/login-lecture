a8bfc9016ef8f41ca15ceaee6b81e7d2ed3516cd
yeojihye <eo1751@hufs-gsuite.kr>
2021-04-19 21:30:02 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들웨어를 등록해주는 메서드.
+
+module.exports = app;
