7183a44c76207af6a36e79cd4479b550e31c7c9a
yeojihye <eo1751@hufs-gsuite.kr>
2021-04-20 12:46:23 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,13 +9,14 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click", register);
 
 function register() {
+    if(!id.value) return alert("아이디를 입력해주십시요.");
+    if (psword == confirmPsword.value) return alert("비밀번호가 일치하지 않습니다.");
+
     const req = {
         id: id.value,
         name: name.value,
         psword: psword.value,
-        confirmPsword: confirmPsword.value,
     };  
-    console.log(req);
 
     fetch("/register", {
         method: "POST",
@@ -33,6 +34,6 @@ function register() {
         }
     })
     .catch((err) => {
-        console.error(new Error("회원가입 중 에러 발생"));
+        console.error("회원가입 중 에러 발생");
     });
 };
