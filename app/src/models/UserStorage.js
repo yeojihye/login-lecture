e5cd6637db97658766a8d13fb99e5e5da5362a66
yeojihye <eo1751@hufs-gsuite.kr>
2021-04-20 11:54:09 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,22 +1,34 @@
 "use strict";
 
 class UserStorage {
-    static #users = {
-        id: ["jihye", "나개발", "김팀장"],
-        psword: ["0199" ,"1234", "123456"],
-        name: ["지혜", "나개발", "김팀장"],
-    };
+  static #users = {
+    id: ["jihye", "나개발", "김팀장"],
+    psword: ["0199", "1234", "123456"],
+    name: ["지혜", "나개발", "김팀장"],
+  };
 
-    static getUsers(...fields) {
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers; 
-    };
-};
+  static getUsers(...fields) {
+    const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
+
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users); // => [id, psword, name]
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
+  }
+}
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,40 +1,25 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
-   home: (req, res) => {
-        res.render("home/index");
-    },
-    login: (req, res) => {
-        res.render("home/login");
-    },
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
 const process = {
-    login: (req, res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id", "psword");
-
-        const response = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-                };
-        };
-
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다."
-        return res.json(response);
-    },
+  login: (req, res) => {
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response);
+  },
 };
 
 module.exports = {
-    output,
-    process,
+  output,
+  process,
 };
-
