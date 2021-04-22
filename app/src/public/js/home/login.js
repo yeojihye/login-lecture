1e7fca0ba3feeecbe214517331d31955ec6f303f
yeojihye <eo1751@hufs-gsuite.kr>
2021-04-22 18:03:25 +0900

    winston 프로젝트에 적용

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,33 +1,37 @@
 "use strict";
 
 const id = document.querySelector("#id"),
-    psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button");
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("#button");
 
 loginBtn.addEventListener("click", login);
 
 function login() {
-    const req = {
-        id: id.value,
-        psword: psword.value,
-    };  
+  if (!id.value) return alert("아이디를 입력해주십시오.");
+  if (!psword.value) return alert("비밀번호를 입력해주십시오.");
 
-    fetch("/login", {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify(req),
-    })
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
     .then((res) => res.json())
     .then((res) => {
-        if (res.success) {
-            location.href = "/";
-        } else {
-            alert(res.msg);
-        }
+      if (res.success) {
+        location.href = "/";
+      } else {
+        if (res.err) return alert(res.err);
+        alert(res.msg);
+      }
     })
     .catch((err) => {
-        console.error(new Error("로그인 중 에러 발생"));
+      console.error("로그인 중 에러 발생");
     });
-};
+}
