7c92132b7a78bb2d94bccfab68f4873a5f727b21
yeojihye <eo1751@hufs-gsuite.kr>
2021-04-22 17:42:10 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -3,9 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
 
 const app = express();
+dotenv.config();
 
 // 라우팅
 const home = require("./src/routes/home");
@@ -16,6 +17,7 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드.
 
